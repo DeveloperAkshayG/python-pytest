84cb550e966e5dc8df751cf653cae107ed5efb3d
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-10-22 23:14:03 +0530

    pytest raises exception example 1

--- test_module02.py
+++ test_module02.py
@@ -1,11 +1,31 @@
-# Test class Example
-class TestMyclass:
-    
-    def test_type(self):
-        assert type(2) == int
+# pytest raises exception
 
-    def test_upper(self):
-        assert(str.upper('python')) == 'PYTHON'
+import pytest
 
-    def test_lower(self):
-        assert (str.lower('PYTHON')) == 'PYTHON'
+def test_case01():
+    with pytest.raises(Exception):
+        assert (1/0)
+
+def test_case02():
+    with pytest.raises(ZeroDivisionError):
+        assert (1/0)
+
+# TO PRINT WHAT TYPE OF EXCEPTION WE ARE GETTING THIS METHOD IS USED
+def test_case03():
+    with pytest.raises(Exception) as exinfo:
+        assert 3 > 3
+    print(str(exinfo))
+
+def test_case04():
+    with pytest.raises(AssertionError):
+        assert 4<3
+
+def func1():
+    raise ValueError('valueError raised via func1')
+
+def test_case05():
+    with pytest.raises(Exception) as exinfo:
+        func1()
+    print(str(exinfo))
+    # to print what type of exception we are getting because of error in function
+    assert str(exinfo.value) == "valueError raised via func1"
