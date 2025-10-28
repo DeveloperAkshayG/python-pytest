cc8f4a1f6f0f98db840e53767e1b76801cc413f2
Akshay Gajare <71394698+DeveloperAkshayG@users.noreply.github.com>
2025-10-28 13:49:00 +0530

    setup and teardown fixtures in pytest

---
+++ test_fixtureteardown.py
@@ -0,0 +1,26 @@
+"""
+fixture setup and teardown using yield
+"""
+
+import pytest
+
+weekdays1=['mon','tue','wed']
+weekdays2=['fri','sat','sun']
+
+@pytest.fixture
+def setup01():
+    # part of setup
+    wk1=weekdays1.copy()
+    wk1.append('thru')
+    """
+    yeild similar to return , except in return statement after return are not executed , while in yield statement after return 
+    are executed 
+    """
+    yield wk1
+    # part of teardown
+    print('\n After yield in setup01 fixture....')
+    wk1.pop()
+
+def test_extendList(setup01):
+    setup01.extend(weekdays2)
+    assert setup01 == ['mon','tue','wed','thru','fri','sat','sun']
